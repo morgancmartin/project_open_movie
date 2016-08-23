4412602358604a889573d6ec4b22d3d1df2025bf
Morgan Martin <morganm5201@gmail.com>
2016-08-23 18:41:57 -0400

    fix controller

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,7 +2,8 @@ class SearchResultsController < ApplicationController
 
   def index
     s = SodaAPI.new
-    s.add_where_like_arg(sr_params[:query_type], sr_params[:query])
+    s.add_title(params[:query])
+    # s.add_where_like_arg(sr_params[:query_type], sr_params[:query])
     @results = s.get
   end
 
